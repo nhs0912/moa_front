0a3fe55b8e502affe28b3feb257a38665bfd8f74
nhs0912 <nhs0912@nate.com>
2026-04-23 12:56:49 +0900

    ing: making entryPage.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,33 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
 import EntryPage from "../pages/EntryPage.vue";
+import uploadPage from "../pages/UploadPage.vue";
+import AccessPage from "../pages/AccessPage.vue";
+import GalleryPage from "../pages/GalleryPage.vue";
 
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
-    name: "Home",
+    redirect: "/e/simple-event",
+  },
+  {
+    path: "/e/:eventCode",
+    name: "entry",
     component: EntryPage,
+  },
+  {
+    path: "/e/:eventCode/upload",
+    name: "upload",
+    component: uploadPage,
+  },
+  {
+    path: "/e/:eventCode/access",
+    name: "access",
+    component: AccessPage,
+  },
+  {
+    path: "/e/:eventCode/gallery",
+    name: "gallery",
+    component: GalleryPage,
   },
 ];
 
