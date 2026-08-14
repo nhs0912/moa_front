43ff5897befa18d1fdd21e8c97bc0a1908974b7e
david Noh <nhs0912@nate.com>
2026-08-14 16:30:41 +0900

    fix: delete cloudflare setting

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
+// import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), cloudflare()],
+  // plugins: [vue(), tailwindcss(), cloudflare()],
+  plugins: [vue(), tailwindcss()],
 
   resolve: {
     alias: {
